cbfa1f0b043ccb197ec05fb8cda3457e4f61c572
Julien JAMET <julienjamet1992@protonmail.com>
2025-05-25 23:28:35 +0200

    add [ get projects ] route

--- src/interfaces/hub/hub.ts
+++ src/interfaces/hub/hub.ts
@@ -4,3 +4,28 @@ export interface PageHeader {
     text: string[];
 };
 /****************************************************/
+
+
+/****************************************[ PROJECT ]*/
+interface Skill {
+    name: string;
+    icon: string;
+};
+
+interface Starting {
+    name: string;
+    link: string;
+};
+
+export interface Project {
+    number: number;
+    name: string;
+    image: string;
+    subject: string;
+    skills: Skill[];
+    difficulty: number;
+    startings: Starting[];
+    category: string;
+    available: boolean;
+};
+/****************************************************/
